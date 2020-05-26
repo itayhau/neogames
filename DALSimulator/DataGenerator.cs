f0c21fbb68da5d79f4bcae56eafbb4909aa5581c
itayhau <itayhau@gmail.com>
2020-05-26 15:21:09 +0300

    Add files via upload

---
+++ DALSimulator/DataGenerator.cs
@@ -0,0 +1,28 @@
+﻿using CoreProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALSimulator
+{
+    static class DataGenerator
+    {
+        static internal List<PurchaseInfo> GenerateData()
+        {
+            return new List<PurchaseInfo>
+            {
+                // should consider that ItemNo will be an auto increment number depend on the result
+                // or perhaps index ID
+
+                new PurchaseInfo { ItemNo = 1, Amount = 1, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 31)},
+                new PurchaseInfo { ItemNo = 2, Amount = 5, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 31)},
+                new PurchaseInfo { ItemNo = 3, Amount = 56, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 32)},
+                new PurchaseInfo { ItemNo = 4, Amount = 22, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 32)},
+                new PurchaseInfo { ItemNo = 5, Amount = 154.5f, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 32)},
+                new PurchaseInfo { ItemNo = 6, Amount = 35.56f, PurchaseDate = new DateTime(2019, 11, 26, 16, 09, 32)}
+            };
+        }
+    }
+}
---
+++ DALSimulator/DataHolder.cs
@@ -0,0 +1,21 @@
+﻿using CoreProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALSimulator
+{
+    public static class DataHolder
+    {
+        public static List<PurchaseInfo> Data { get; set; }
+
+        public static Dictionary<string, int> MaprquestIdToLastIndex { get; private set; }
+
+        static DataHolder()
+        {
+            MaprquestIdToLastIndex = new Dictionary<string, int>();
+        }
+    }
+}
